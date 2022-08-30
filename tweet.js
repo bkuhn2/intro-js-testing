600dd2145f1363cdd939cf2efa8586eac11373fb
Brett Kuhn <brettkuhn87@gmail.com>
2022-08-30 14:48:23 -0600

    Complete tweet tests

--- tweet.js
+++ tweet.js
@@ -1,5 +1,27 @@
 class Tweet {
-
+  constructor(handle, tweetText, link){
+    this.sender = handle;
+    this.content = tweetText;
+    if (link === undefined) {
+      this.link = "no link provided"
+    } else {
+    this.link = link
+    }
+    this.likes = 0
+    this.retweets = 0
+    this.replies = []
+    this.replyCount = 0
+  }
+  like(){
+    this.likes++
+  }
+  retweet(){
+    this.retweets++
+  }
+  reply(replyText){
+    this.replies.push(replyText)
+    this.replyCount++
+  }
 }
 
 module.exports = Tweet;
